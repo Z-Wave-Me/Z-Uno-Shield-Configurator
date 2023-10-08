a5c2ec5be8e0b27ed77f5d500f63d8b7476081d9
Xel4ek <l-baal@yandex.ru>
2023-10-08 15:29:04 +0300

    device types

---
+++ apps/configurator/src/app/shared/device.model.ts
@@ -0,0 +1,12 @@
+export enum DeviceType {
+  SwitchBinary = 'switchBinary',
+  Thermostat = 'thermostat',
+  SwitchMultilevel = 'switchMultilevel',
+  SwitchColor = 'switchColor',
+  SensorBinary = 'sensorBinary',
+  SensorMultilevel = 'sensorMultilevel',
+  DHT = 'dht',
+  DS18B20 = 'ds18b20',
+  UART = 'uart',
+  RS485 = 'rs485',
+}
