c51e8fe601f507c0209472b125ad00e33dcadc15
amatilda <kpk@z-wave.me>
2024-02-20 21:29:30 +0300

    add style

--- libs/uploader/src/lib/upload-button/upload-button.component.ts
+++ libs/uploader/src/lib/upload-button/upload-button.component.ts
@@ -1,7 +1,14 @@
 import { Component, Input } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MatDialogState } from '@angular/material/dialog';
 import { UploadModalComponent } from '../upload-modal/upload-modal.component';
 import { ZUnoCompiler } from '../z-uno-compiler';
+
+function _style_cursor_change(id:string, severity: string): void {
+  const id_dialog:HTMLElement|null = document.getElementById(id);
+  if (id_dialog == null)
+    return ;
+  id_dialog.style.cursor = severity === "error" ? "auto" : "progress";
+}
 
 @Component({
   selector: 'configurator-upload-button',
@@ -12,17 +19,24 @@ export class UploadButtonComponent {
   @Input()
   code: string = '';
 
+  private zUnoCompilerDialogProgress: MatDialogRef<UploadModalComponent>|undefined = undefined;
   private readonly zUnoCompiler = ZUnoCompiler()
   constructor(
-    private readonly matDialog: MatDialog
+    private readonly matDialog: MatDialog,
   ) {
     this.zUnoCompiler.setProgress((severity: string, message: string) => {
-      this.matDialog.open(UploadModalComponent, {
+      if (this.zUnoCompilerDialogProgress == undefined || this.zUnoCompilerDialogProgress.getState() != MatDialogState.OPEN) {
+        this.zUnoCompilerDialogProgress = this.matDialog.open(UploadModalComponent, {
         data: {
           severity,
           message,
         },
       });
+      _style_cursor_change(this.zUnoCompilerDialogProgress.id, severity);
+      return ;
+    }
+    this.zUnoCompilerDialogProgress.componentInstance.data.message += "<br />" + message;
+    _style_cursor_change(this.zUnoCompilerDialogProgress.id, severity);
     })
   }
 
