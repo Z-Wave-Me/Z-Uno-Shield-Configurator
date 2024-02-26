8711d9f062ea502aaa4879b4f4aac5236f43a8e4
Ицкович Алексей Анатольевич <itskovichaa@pgk.ru>
2024-02-26 06:51:49 +0300

    upload

--- libs/uploader/src/lib/upload-modal/upload-modal.component.ts
+++ libs/uploader/src/lib/upload-modal/upload-modal.component.ts
@@ -1,14 +1,13 @@
-import { Component, ElementRef, HostBinding, HostListener, Inject, OnInit, ViewChild } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef, MatDialogState } from '@angular/material/dialog';
+import { AfterViewInit, Component, ElementRef, HostBinding, Inject, ViewChild } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ZUnoCompilerClass } from '../../ZUnoCompiler';
-import { verifyHostBindings } from '@angular/compiler';
 
 @Component({
   selector: 'configurator-upload-modal',
   templateUrl: './upload-modal.component.html',
   styleUrls: ['./upload-modal.component.scss'],
 })
-export class UploadModalComponent implements OnInit {
+export class UploadModalComponent implements AfterViewInit {
   private readonly compiler = new ZUnoCompilerClass(this.data.code, this.data.freq, true, 50, (severity: string, message: string) => {
     this.items.push({severity, message})
   });
@@ -36,11 +35,11 @@ export class UploadModalComponent implements OnInit {
     this.matDialogRef.close();
   }
 
-  public ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.isLoading = true;
     this.compiler.getWait().then( result => {
         if (result && result.smart_qr && result?.dsk) {
-         this.dskHelp = "Use this QR-code to include your device using Z-Wave Smart Start or triple click BTN button";
+          this.dskHelp = "Use this QR-code to include your device using Z-Wave Smart Start or triple click BTN button";
           this.dsk = result.dsk;
           const qrCode = this.qr.nativeElement;
           if (qrCode) {
