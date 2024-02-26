1c81ef4d04c84505a25f610f530d9b178c5577bd
Ицкович Алексей Анатольевич <itskovichaa@pgk.ru>
2024-02-26 06:45:03 +0300

    upload

--- libs/uploader/src/lib/upload-control/upload-control.component.ts
+++ libs/uploader/src/lib/upload-control/upload-control.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
-import { MatDialog, MatDialogRef, MatDialogState } from '@angular/material/dialog';
+import { MatDialog} from '@angular/material/dialog';
 import { UploadModalComponent } from '../upload-modal/upload-modal.component';
-import { ZUnoCompilerClass, ZUnoCompilerLoadSketchOutProt } from '../../ZUnoCompiler';
+import { ZUnoCompilerClass } from '../../ZUnoCompiler';
 import { PreventDirective } from '../prevent/prevent.directive';
 
 @Component({
@@ -11,11 +11,6 @@ import { PreventDirective } from '../prevent/prevent.directive';
   hostDirectives: [PreventDirective],
 })
 export class UploadControlComponent {
-  private zUnoCompilerClass?: ZUnoCompilerClass;
-
-  private progress_dialog: MatDialogRef<UploadModalComponent>|undefined = undefined;
-  // private items:Array<{"severity":string, "message": string}> = [];
-  // private b_run:boolean = false;
 
   public selectedFreq: string = '';
 
@@ -39,57 +34,5 @@ export class UploadControlComponent {
         code: this.code,
       },
     });
-    // if (this.zUnoCompilerClass != undefined) {
-    //   // this.zUnoCompilerClass.cancel();
-    //   // this.zUnoCompilerClass = undefined;
-    //   if (this.progress_dialog == undefined || this.progress_dialog.getState() != MatDialogState.OPEN) {
-    //     this.progress_dialog = this.matDialog.open(UploadModalComponent, {
-    //       data: {"items": this.items, "dsk_help": undefined, "dsk": undefined, },
-    //     });
-    //   }
-    //   return ;
-    // }
-    // this.items = [];
-    // let freq:string|null = null;
-    // const e = document.getElementById("select_freq_component_id");
-    // if (e != null) {
-    //   if (ZUnoCompilerClass.getFreqList().includes(e.innerText) == true)
-    //     freq = e.innerText;
-    // }
-    // const self:UploadControlComponent = this;
-    // this.zUnoCompilerClass = new ZUnoCompilerClass(this.code, freq, true, 50, (severity: string, message: string) => {
-    //   if (this.progress_dialog == undefined || this.progress_dialog.getState() != MatDialogState.OPEN) {
-    //     this.progress_dialog = this.matDialog.open(UploadModalComponent, {
-    //       data: {"items": [], "dsk_help": undefined, "dsk": undefined, },
-    //     });
-    //   }
-    //   this.items.push({"severity":severity, "message":message});
-    //   this.progress_dialog.componentInstance.data.items = this.items;
-    // });
-    // this.zUnoCompilerClass.getWait().then( result => {
-    //     if (self.progress_dialog == undefined || self.progress_dialog.getState() != MatDialogState.OPEN) {
-    //       self.progress_dialog = self.matDialog.open(UploadModalComponent, { data: {"items": [], "dsk_help": undefined, "dsk": undefined, },
-    //       });
-    //     }
-    //     if (result != undefined && result["smart_qr"] != undefined && result["dsk"] != undefined) {
-    //       self.progress_dialog.componentInstance.data.dsk_help = "Use this QR-code to include your device using Z-Wave Smart Start or triple click BTN button";
-    //       self.progress_dialog.componentInstance.data.dsk = result["dsk"];
-    //       const qr_code:HTMLElement|null =  document.getElementById("configurator-upload-button_qr-code");
-    //       if (qr_code == null || self.zUnoCompilerClass == undefined) {
-    //         self.zUnoCompilerClass = undefined;
-    //         return ;
-    //       }
-    //       const res:boolean = self.zUnoCompilerClass.drawQR(qr_code, result["smart_qr"]);
-    //       if (res != false) {
-    //         qr_code.style.display ='';
-    //         self.zUnoCompilerClass = undefined;
-    //         return ;
-    //       }
-    //     }
-    //   },
-    //   error => {
-    //     self.zUnoCompilerClass = undefined;
-    //   }
-    // );
   }
 }
--- libs/uploader/src/lib/upload-modal/upload-modal.component.ts
+++ libs/uploader/src/lib/upload-modal/upload-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, HostBinding, HostListener, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef, MatDialogState } from '@angular/material/dialog';
 import { ZUnoCompilerClass } from '../../ZUnoCompiler';
 
@@ -12,8 +12,14 @@ export class UploadModalComponent implements OnInit {
     this.items.push({severity, message})
   });
 
+  @HostBinding('class.loading')
+  public get loading() {
+    return this.isLoading;
+  }
+
+  public isLoading = false;
   public items: Severity[] = [];
-  public dsk_help: string = '';
+  public dskHelp: string = '';
   public dsk: string = '';
   constructor(
     @Inject(MAT_DIALOG_DATA)
@@ -27,40 +33,21 @@ export class UploadModalComponent implements OnInit {
   }
 
   public ngOnInit(): void {
-    // const compiler = new ZUnoCompilerClass(this.data.code, this.data.freq, true, 50, (severity: string, message: string) => {
-    //   this.items.push({severity, message})
-    //   // if (this.progress_dialog == undefined || this.progress_dialog.getState() != MatDialogState.OPEN) {
-    //   //   this.progress_dialog = this.matDialog.open(UploadModalComponent, {
-    //   //     data: {"items": [], "dsk_help": undefined, "dsk": undefined, },
-    //   //   });
-    //   // }
-    //   // this.items.push({"severity":severity, "message":message});
-    //   // this.progress_dialog.componentInstance.data.items = this.items;
-    // });
+    this.isLoading = true;
     this.compiler.getWait().then( result => {
         if (result && result.smart_qr && result?.dsk) {
-         this.dsk_help = "Use this QR-code to include your device using Z-Wave Smart Start or triple click BTN button";
+         this.dskHelp = "Use this QR-code to include your device using Z-Wave Smart Start or triple click BTN button";
           this.dsk = result.dsk;
           const qr_code:HTMLElement|null =  document.getElementById("configurator-upload-button_qr-code");
-          // if (qr_code == null || self.zUnoCompilerClass == undefined) {
-          //   self.zUnoCompilerClass = undefined;
-          //   return ;
-          // }
           if (qr_code) {
             this.compiler.drawQR(qr_code, result.smart_qr);
-            qr_code.style.display =''
+            qr_code.style.display ='';
           }
-          // if (res != false) {
-          //   qr_code.style.display ='';
-          //   self.zUnoCompilerClass = undefined;
-          //   return ;
-          // }
         }
       },
-      error => {
-        // self.zUnoCompilerClass = undefined;
-      }
-    );
+    ).finally(() => {
+      this.isLoading = false;
+    });
   }
 }
 
